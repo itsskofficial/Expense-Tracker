635fc278c2cc35daa09c74e6a85b1fb79c651c58
SK <sarthakkarandikar03@gmail.com>
2022-12-01 11:45:59 +0000

    Dec 1, 2022, 5:15 PM

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,7 @@
 import '../css/ExpenseItem.css'
 
 function ExpenseItem(props) {
-    month=props.expense.date.toLocaleString('en-US', {})
+    month=props.expense.date.toLocaleString('en-US', {month:'long'})                                                                
     return (
         <div className="expense-item">
             <div className="expense-item-date">
