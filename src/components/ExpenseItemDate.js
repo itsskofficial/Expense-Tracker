c29d8c2bb1f393e7a29980355feacff6be03f923
SK <sarthakkarandikar03@gmail.com>
2022-12-01 11:59:52 +0000

    Dec 1, 2022, 5:29 PM

--- src/components/ExpenseItemDate.js
+++ src/components/ExpenseItemDate.js
@@ -1,3 +1,5 @@
+
+
 function ExpenseItemDate(props) {
     const month=props.expense.date.toLocaleString('en-US', {month:'long'})                                                               
     const day=props.expense.date.toLocaleString('en-US', {day:'2-digit'})                                                               
