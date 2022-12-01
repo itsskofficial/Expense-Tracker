6c9ec0f874696898dc93546de7de93715f5ca6e1
SK <sarthakkarandikar03@gmail.com>
2022-12-01 12:00:19 +0000

    Dec 1, 2022, 5:30 PM

--- src/components/ExpenseItemDate.js
+++ src/components/ExpenseItemDate.js
@@ -6,6 +6,16 @@ function ExpenseItemDate(props) {
     const year = props.expense.date.getFullYear() 
     
     return (){
-        
+        <div className="expense-item-date">
+            <div className="expense-item-date-month">
+                {month}
+            </div>
+            <div className="expense-item-date-day">
+                {day}
+            </div>
+            <div className="expense-item-date-year">
+                {year}
+            </div>
+            </div>
     }
 }
