48fba701a05cb5c1265892f00ec5f1f658ee3e65
SK <sarthakkarandikar03@gmail.com>
2022-12-01 11:59:57 +0000

    Dec 1, 2022, 5:29 PM

--- src/components/ExpenseItemDate.js
+++ src/components/ExpenseItemDate.js
@@ -5,4 +5,7 @@ function ExpenseItemDate(props) {
     const day=props.expense.date.toLocaleString('en-US', {day:'2-digit'})                                                               
     const year = props.expense.date.getFullYear() 
     
+    return (){
+        
+    }
 }
