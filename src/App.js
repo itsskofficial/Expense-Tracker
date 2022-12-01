a29763b82234a04307a00da9c9eaa6ecf78a23b3
SK <sarthakkarandikar03@gmail.com>
2022-12-01 10:09:09 +0000

    Dec 1, 2022, 3:39 PM

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import ExpenseItem from "./components/ExpenseItem"
+import './index.css'
+
+function App() {
+  let expenses = [
+    {
+      date:new Date(2021, 12, 3),
+      title:"Ojaswini",
+      price:500
+    },
+    {
+      date:new Date(2021, 12, 3),
+      title:"Ojaswini",
+      price:500
+    },
+    {
+      date:new Date(2021, 12, 3),
+      title:"Ojaswini",
+      price:500
+    }
+  ]
+
+  return (
+    <div>
+        <ExpenseItem expense={expenses[0]}>
+      </ExpenseItem>
+         <ExpenseItem expense={expenses[1]}>
+    </ExpenseItem>
+    <ExpenseItem expense={expenses[2]}>
+    </ExpenseItem>
+    </div>
+  )
+}
+
+export default App
+
