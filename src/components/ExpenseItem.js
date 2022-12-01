5b5157322e76441bfed022e3ceeecfbddc3ab85a
SK <sarthakkarandikar03@gmail.com>
2022-12-01 12:00:13 +0000

    Dec 1, 2022, 5:30 PM

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,17 +3,7 @@ import '../css/ExpenseItem.css'
 function ExpenseItem(props) {                                                             
     return (
         <div className="expense-item">
-            <div className="expense-item-date">
-                <div className="expense-item-date-month">
-                    {month}
-                </div>
-                <div className="expense-item-date-day">
-                    {day}
-                </div>
-                <div className="expense-item-date-year">
-                    {year}
-                </div>
-            </div>
+            
             <div className="expense-item-info">
                 <div className="expense-item-title">
                     {props.expense.title}  
