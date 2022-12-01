7a02a5ca4dd082c583526665bcd13f1a3d3272f9
SK <sarthakkarandikar03@gmail.com>
2022-12-01 08:23:23 +0000

    Dec 1, 2022, 1:53 PM

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -8,6 +8,9 @@ function ExpenseItem() {
                 <div>
                     
                 </div>
+                <div>
+                Price
+            </div>
                 Title
             </div>
         </div>
