8162b3d1fa6b69d6968b6666006800b665098728
SK <sarthakkarandikar03@gmail.com>
2022-12-01 09:22:38 +0000

    Dec 1, 2022, 2:52 PM

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,7 +2,8 @@ import '../css/ExpenseItem.css'
 
 function ExpenseItem() {
     const expenseDate = new Date(2021, 3, 28)
-    const expenseTitle="Ojaswini's Bracelet"
+    const expenseTitle = "Ojaswini's Bracelet"
+    const expensePrice=""
 
     return (
         <div className="expense-item">
