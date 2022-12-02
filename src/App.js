1a537513b0cb61ad725e22bf0d70d99702b3264a
SK <sarthakkarandikar03@gmail.com>
2022-12-02 06:20:45 +0000

    Dec 2, 2022, 11:50 AM

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
-import './index.css'
-import './css/Card.css'
-import Card from "./components/Card"
-import ExpenseItem from "./components/ExpenseItem"
+import "./index.css";
+import "./css/Card.css";
+import Card from "./components/UI/Card";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   let expenses = [
     {
-      date:new Date(2021, 12, 3),
-      title:"Ojaswini",
-      price:500
+      date: new Date(2021, 12, 3),
+      title: "Ojaswini",
+      price: 500,
     },
     {
-      date:new Date(2021, 12, 3),
-      title:"Ojaswini",
-      price:500
+      date: new Date(2021, 12, 3),
+      title: "Ojaswini",
+      price: 500,
     },
     {
-      date:new Date(2021, 12, 3),
-      title:"Ojaswini",
-      price:500
-    }
-  ]
+      date: new Date(2021, 12, 3),
+      title: "Ojaswini",
+      price: 500,
+    },
+  ];
 
   return (
     <Card className="expense-container">
-      <ExpenseItem expense={expenses[0]}>
-      </ExpenseItem>
-      <ExpenseItem expense={expenses[1]}>
-      </ExpenseItem>
-      <ExpenseItem expense={expenses[2]}>
-      </ExpenseItem>
+      <ExpenseItem expense={expenses[0]}></ExpenseItem>
+      <ExpenseItem expense={expenses[1]}></ExpenseItem>
+      <ExpenseItem expense={expenses[2]}></ExpenseItem>
     </Card>
-  )
+  );
 }
 
-export default App
-
+export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,18 @@
-import '../css/ExpenseItem.css'
-import '../css/Card.css'
-import Card from './Card'
-import ExpenseItemDate from './ExpenseItemDate'
+import "../css/ExpenseItem.css";
+import "../css/Card.css";
+import Card from "./UI/Card";
+import ExpenseItemDate from "./ExpenseItemDate";
 
-function ExpenseItem(props) {                                                             
-    return (
-        <Card className="expense-item">
-            <ExpenseItemDate expenseDate={props.expense.date}>
-            </ExpenseItemDate>
-            <div className="expense-item-info">
-                <div className="expense-item-title">
-                    {props.expense.title}  
-                </div>
-                <div className="expense-item-price">
-                    ${props.expense.price}
-                </div>
-            </div>
-        </Card>
-    )
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseItemDate expenseDate={props.expense.date}></ExpenseItemDate>
+      <div className="expense-item-info">
+        <div className="expense-item-title">{props.expense.title}</div>
+        <div className="expense-item-price">${props.expense.price}</div>
+      </div>
+    </Card>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
