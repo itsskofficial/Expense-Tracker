83ac6286330dbb333d19c4d133c7b724b7116b1b
SK <sarthakkarandikar03@gmail.com>
2022-12-02 05:35:23 +0000

    Dec 2, 2022, 11:05 AM

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,7 @@
 function Card(props) {
     return (
-        <div className={"card " + props.className}>{props.children}</div>
+        <div className={"card " + props.className}>
+            {props.children}\
+        </div>
     )
 }
