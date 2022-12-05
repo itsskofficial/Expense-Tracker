04c17d4393aebbbf7faaa2c3ac041e842344514c
SK <sarthakkarandikar03@gmail.com>
2022-12-05 04:12:35 +0000

    Dec 5, 2022, 9:42 AM

--- src/components/Expenses/NewExpenseItem.js
+++ src/components/Expenses/NewExpenseItem.js
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import './css/NewExpenseItem.css'
 
 const NewExpenseItem = function () {
-    const enteredTitle,setEnteredTitle=useState('new-expense-item-title-input')
+    const enteredTitle, setEnteredTitle = useState('new-expense-item-title-input')
+    
+    const on
     return (
         <div>
             <form>
