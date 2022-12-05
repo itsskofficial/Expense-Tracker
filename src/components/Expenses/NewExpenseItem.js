2ba057bc7e8e09ad57e5d5dc42305bd4d0bbeedc
SK <sarthakkarandikar03@gmail.com>
2022-12-05 03:22:33 +0000

    Dec 5, 2022, 8:52 AM

--- src/components/Expenses/NewExpenseItem.js
+++ src/components/Expenses/NewExpenseItem.js
@@ -8,7 +8,7 @@ const NewExpenseItem = function () {
                         <label for="new-expense-item-title-input">
                             Title
                         </label>        
-                        
+                        <input type="text" class
                     </div>
                 </div>
             </form>
