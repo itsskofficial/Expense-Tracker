ee9bedb8807898a501df2814e1d77bbcfdce4d28
SK <sarthakkarandikar03@gmail.com>
2022-12-05 04:26:34 +0000

    Dec 5, 2022, 9:56 AM

--- src/components/Expenses/NewExpenseItem.js
+++ src/components/Expenses/NewExpenseItem.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './css/NewExpenseItem.css'
 
 const NewExpenseItem = function () {
-    const [enteredTitle, setEnteredTitle] = useState('new-expense-item-title-input')
+    const [enteredTitle, setEnteredTitle] = useState('')
     const [enteredAmount, setEnteredAmount] = useState('')
     const [enteredDate, setEnteredDate] = useState('')
     
