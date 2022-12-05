305b329a21fec224785dd9eece6252b458c3d829
SK <sarthakkarandikar03@gmail.com>
2022-12-05 04:13:05 +0000

    Dec 5, 2022, 9:43 AM

--- src/components/Expenses/NewExpenseItem.js
+++ src/components/Expenses/NewExpenseItem.js
@@ -4,7 +4,9 @@ import './css/NewExpenseItem.css'
 const NewExpenseItem = function () {
     const enteredTitle, setEnteredTitle = useState('new-expense-item-title-input')
     
-    const titlehangeHandler = (event)
+    const titlehangeHandler = (event) => {
+        
+    }
     return (
         <div>
             <form>
