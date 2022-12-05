20248f937ce01205489543188c59d1ce007508bb
SK <sarthakkarandikar03@gmail.com>
2022-12-05 03:22:09 +0000

    Dec 5, 2022, 8:52 AM

--- src/components/Expenses/NewExpenseItem.js
+++ src/components/Expenses/NewExpenseItem.js
@@ -5,7 +5,7 @@ const NewExpenseItem = function () {
             <form>
                 <div className="new-expense-item-controls">
                     <div className="new-expense-item-control">
-                        <label for="new-expense-item-title"        
+                        <label for="new-expense-item-"        
                     </div>
                 </div>
             </form>
