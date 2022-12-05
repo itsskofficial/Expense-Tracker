83ff3a175bfd32263ea9ac93c43c56e097ca11f5
SK <sarthakkarandikar03@gmail.com>
2022-12-05 03:27:50 +0000

    Dec 5, 2022, 8:57 AM

--- src/components/Expenses/NewExpenseItem.js
+++ src/components/Expenses/NewExpenseItem.js
@@ -16,6 +16,12 @@ const NewExpenseItem = function () {
                         </label>        
                         <input type="text" className="new-expense-item-amount-input" id="new-expense-item-amount-input" min="0.01" step="0.01"></input>
                     </div>
+                    <div className="new-expense-item-control">
+                        <label for="new-expense-item-amount-input" className="new-expense-item-label">
+                            Amount
+                        </label>        
+                        <input type="text" className="new-expense-item-amount-input" id="new-expense-item-amount-input" min="0.01" step="0.01"></input>
+                    </div>
                 </div>
             </form>
         </div>
