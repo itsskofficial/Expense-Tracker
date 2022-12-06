79bb50d9f681f1a2d735a379796fce8391ad1d32
SK <sarthakkarandikar03@gmail.com>
2022-12-06 09:25:53 +0000

    Dec 6, 2022, 2:55 PM

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -4,8 +4,11 @@ const ExpenseFilter = function () {
     return (
         <div className="expense-filter">
             <div className="expense-filter-control">
-                <label className="expense-filter-label">Filter By Year</label>
-                <select name
+                <label className="expense-filter-label">
+                    Filter By Year</label>
+                <select className="expense-filter-selector">
+
+                </select>
             </div>
            
 
