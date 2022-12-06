e2b00afab650c72a192440c8692885e192614d8f
SK <sarthakkarandikar03@gmail.com>
2022-12-06 10:07:29 +0000

    Dec 6, 2022, 3:37 PM

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -19,8 +19,8 @@ const ExpenseFilter = function (props) {
                 </label>
                 <select className="expense-filter-selector">
                     <option value="2019">2019</option>
-                    <option value="2020" onClick={yearSelectHandler}>2020</option>
-                    <option value="2021" onClick={yearSelectHandler}>2021</option>
+                    <option value="2020">2020</option>
+                    <option value="2021">2021</option>
                     <option value="2022" onClick={yearSelectHandler}>2022</option>
                 </select>
             </div>
