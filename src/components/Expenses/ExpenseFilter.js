b91543291b37efa7b835a6373e1753754b3be718
SK <sarthakkarandikar03@gmail.com>
2022-12-06 09:26:50 +0000

    Dec 6, 2022, 2:56 PM

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -10,7 +10,7 @@ const ExpenseFilter = function () {
                 <select className="expense-filter-selector">
                     <option value="2019"></option>
                     <option value="2020"></option>
-                    
+                    <option value="20"
                 </select>
             </div>
            
