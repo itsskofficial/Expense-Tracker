fb02779404f683a41d1d18af58d4cd5d0c8ce584
SK <sarthakkarandikar03@gmail.com>
2022-12-06 08:00:27 +0000

    Dec 6, 2022, 1:30 PM

--- src/components/Expenses/NewExpenseItem.js
+++ src/components/Expenses/NewExpenseItem.js
@@ -17,7 +17,7 @@ const NewExpenseItem = function () {
     }
     return (
         <div>
-            <form>
+            <form onSubmit={submitHandler}>
                 <div className="new-expense-item-controls">
                     <div className="new-expense-item-control">
                         <label for="new-expense-item-title-input" className="new-expense-item-label">
