3d2b14907f774fc21776985982dee0b985b1c1d3
SK <sarthakkarandikar03@gmail.com>
2022-12-07 10:00:18 +0000

    Dec 7, 2022, 3:30 PM

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
         <ExpenseFilter onChangeFilter={changeFilterHandler}></ExpenseFilter>
       </Card>
       <Card className="expense-container">
-        {}
+        {ex}
         {/* <ExpenseItem expense={expenses[0]}></ExpenseItem>
         <ExpenseItem expense={expenses[1]}></ExpenseItem>
         <ExpenseItem expense={expenses[2]}></ExpenseItem> */}
