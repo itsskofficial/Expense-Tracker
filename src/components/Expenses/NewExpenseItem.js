b0c56d71272cbd0403fa7873e159b1761f6892bb
SK <sarthakkarandikar03@gmail.com>
2022-12-10 13:20:39 +0000

    Dec 10, 2022, 6:50 PM

--- src/components/Expenses/NewExpenseItem.js
+++ src/components/Expenses/NewExpenseItem.js
@@ -88,7 +88,17 @@ const NewExpenseItem = function (props) {
             </div>
         )
     }
-
+    
+    const [newExpenseForm, setNewExpenseForm] = useState(
+        <div className="new-expense-item-actions">
+            <div className="new-expense-item-action">
+                <button className="new-expense-item-button" onClick={addNewExpenseHandler}>
+                    Add New Expense
+                </button>
+            </div>
+        </div>
+    )
+    
     return (
         <div>
             {newExpenseForm}
