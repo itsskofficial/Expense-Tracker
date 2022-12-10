8f81ffa7d672a83691a6c5b006f2f18b00954d17
SK <sarthakkarandikar03@gmail.com>
2022-12-10 13:18:16 +0000

    Dec 10, 2022, 6:48 PM

--- src/components/Expenses/NewExpenseItem.js
+++ src/components/Expenses/NewExpenseItem.js
@@ -98,7 +98,7 @@ const NewExpenseItem = function (props) {
             </div>
     }
 
-    newExpenseForm =
+    setNewExpenseForm(
         <div className="new-expense-item-actions">
             <div className="new-expense-item-action">
                 <button className="new-expense-item-button" onClick={addNewExpenseHandler}>
