0242185c07636a082478d60f256d6522d18acf2f
SK <sarthakkarandikar03@gmail.com>
2022-12-10 12:46:59 +0000

    Dec 10, 2022, 6:16 PM

--- src/components/Expenses/NewExpenseItem.js
+++ src/components/Expenses/NewExpenseItem.js
@@ -60,7 +60,7 @@ const NewExpenseItem = function (props) {
                     </div>
                     <div className="new-expense-item-actions">
                         <div className="new-expense-item-action">
-                            <button className="new-expense-item-button" onClick={}>
+                            <button className="new-expense-item-button" onClick={cancelNewExpense}>
                                 Cancel
                             </button>
                         </div>
