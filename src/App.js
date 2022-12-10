37914d5fa135e1746228a8dda18d52efb93e17c2
SK <sarthakkarandikar03@gmail.com>
2022-12-10 12:29:58 +0000

    Dec 10, 2022, 5:59 PM

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
         <ExpenseFilter onChangeFilter={changeFilterHandler}></ExpenseFilter>
       </Card>
       <Card className="expense-container">
-      
+        {expensesContent}
       </Card>
     </div>
   );
