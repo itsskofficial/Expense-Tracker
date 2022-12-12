0182bd0ad6dd20424a2a1628aebeef0e37193755
SK <sarthakkarandikar03@gmail.com>
2022-12-12 06:29:35 +0000

    Dec 12, 2022, 11:59 AM

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,7 +3,7 @@ import './css/Chart.css'
 
 const Chart = (props) => {
     <div className="chart">
-        {props.dataPoints.map(dataPoint= <ChartBar></Chart>)}
+        {props.dataPoints.map(dataPoint= <ChartBar ></ChartBar>)}
     </div>
 }
 
