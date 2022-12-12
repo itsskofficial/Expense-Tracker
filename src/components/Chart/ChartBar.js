e2269033e0970da9862f0280f1abf9d4a9aa4c88
SK <sarthakkarandikar03@gmail.com>
2022-12-12 06:45:47 +0000

    Dec 12, 2022, 12:15 PM

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -2,6 +2,8 @@ import './css/ChartBar.css'
 
 const ChartBar = (props) => {
     return (
-        <div className='chart-bar'
+        <div className='chart-bar'>
+            <div className='cha'
+        </div>
     )
 }
