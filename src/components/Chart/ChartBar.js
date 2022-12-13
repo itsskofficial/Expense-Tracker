6f3d2057879fe7068f6819b6933139634a786390
SK <sarthakkarandikar03@gmail.com>
2022-12-13 06:18:49 +0000

    Dec 13, 2022, 11:48 AM

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -3,7 +3,7 @@ import './css/ChartBar.css'
 const ChartBar = (props) => {
     let barFillHeight = "0%"
     if (props.max > 0) {
-        barFillHeight = Math.round()
+        barFillHeight = Math.round(props.value/props)
     }
     return (
         <div className='chart-bar'>
