b4fe4e183f4910b659fa9c819e4b4ee4556e6e72
SK <sarthakkarandikar03@gmail.com>
2022-12-13 06:39:38 +0000

    Dec 13, 2022, 12:09 PM

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,7 +2,7 @@ import ChartBar from './ChartBar'
 import './css/Chart.css'
 
 const Chart = (props) => {
-    dataPointValues = props.dataPoints.map(dataPoint=>dataPoint.value)
+    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
     <div className="chart">
         {props.dataPoints.map((dataPoint) => <ChartBar key={dataPoint.value} value={dataPoint.value} maxValue={null} label={dataPoint.label}></ChartBar>)}
     </div>
