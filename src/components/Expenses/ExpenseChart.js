9abc3fe1c732b11d0037f2c3e64fb99ada401c45
SK <sarthakkarandikar03@gmail.com>
2022-12-13 06:27:34 +0000

    Dec 13, 2022, 11:57 AM

---
+++ src/components/Expenses/ExpenseChart.js
@@ -0,0 +1,17 @@
+const ExpenseChart = (props) => {
+    const chartDataPoints = [
+        {label:'Jan',value:0},
+        {label:'Jan',value:0},
+        {label:'Jan',value:0},
+        {label:'Jan',value:0},
+        {label:'Jan',value:0},
+        {label:'Jan',value:0},
+        {label:'Jan',value:0},
+        {label:'Jan',value:0},
+        {label:'Jan',value:0},
+        {label:'Jan',value:0},
+        {label:'Jan',value:0},
+        {label:'Jan',value:0},
+        {label:'Jan',value:0},
+    ]
+}
