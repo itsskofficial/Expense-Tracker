459195aec1545756d7b987a26fb6c043094c41a9
SK <sarthakkarandikar03@gmail.com>
2022-12-13 06:40:21 +0000

    Dec 13, 2022, 12:10 PM

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -5,8 +5,8 @@ const Chart = (props) => {
     const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value)
     return (
         <div className="chart">
-        {props.dataPoints.map((dataPoint) => <ChartBar key={dataPoint.value} value={dataPoint.value} maxValue={null} label={dataPoint.label}></ChartBar>)}
-        /div>
+            {props.dataPoints.map((dataPoint) => <ChartBar key={dataPoint.value} value={dataPoint.value} maxValue={null} label={dataPoint.label}></ChartBar>)}
+        <div/div>
     )
     
 }
