912b01eb53f4e164ee1654b679418f173e3ff3c8
SK <sarthakkarandikar03@gmail.com>
2022-12-13 06:38:00 +0000

    Dec 13, 2022, 12:08 PM

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,6 +2,7 @@ import ChartBar from './ChartBar'
 import './css/Chart.css'
 
 const Chart = (props) => {
+    dataPointValues
     <div className="chart">
         {props.dataPoints.map((dataPoint) => <ChartBar key={dataPoint.value} value={dataPoint.value} maxValue={null} label={dataPoint.label}></ChartBar>)}
     </div>
